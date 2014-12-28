44d7a1dd7e495cc622b1226af37b2e874926b89d
wyozi <joonasmertanen@gmail.com>
2014-12-28 03:54:19 +0200

    Add Bootil headers and make gaceio use them

---
+++ README.md
@@ -0,0 +1,22 @@
+Binary IO module for Garry's Mod.
+
+## API
+Similar to http://wiki.garrysmod.com/page/Category:file
+```
+table   gaceio.List(name) // Returns a table of file/folder names
+        gaceio.CreateDir(name)
+        gaceio.Delete(name)
+boolean gaceio.Exists(name)
+boolean gaceio.IsDir(name)
+string  gaceio.Read(name)
+number  gaceio.Size(name)
+number  gaceio.Time(name)
+        gaceio.Write(name, text)
+```
+All functions return ```false, error_string``` on error, which means you might have to check for existence of ```error_string``` when using ```gaceio.Exists(name)``` or other functions that return a boolean.
+
+## Usage
+Grab binaries from Downloads tab, dump them into garrysmod/lua/bin, call ```require("gaceio")``` and have fun.
+
+## Compilation
+You need Garry's Bootil https://github.com/garrynewman/bootil
--- src/gm_gaceio.cpp
+++ src/gm_gaceio.cpp
@@ -1,268 +1,157 @@
 #include "GarrysMod/Lua/Interface.h"
-
-#ifdef _WIN32
-#include <direct.h>
-#include <dirent_win.h>
-#endif
-
-#ifdef __linux__
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h> 
-#include <string.h>
-#include <errno.h> 
-#include <dirent.h>
-#include <stdlib.h>
-#endif
-
-#include <stdio.h>
-
-#include <iostream>
-#include <fstream>
-#include <streambuf>
-#include <sstream> // for ostringstream
+#include "Bootil/Bootil.h"
 
 using namespace GarrysMod::Lua;
 
 int LuaFunc_ListDir( lua_State* state )
 {
-	if ( LUA->IsType( 1, Type::STRING ) )
+	if ( !LUA->IsType( 1, Type::STRING ) )
 	{
-		const char* path = LUA->GetString( 1 );
-
-		DIR *dir;
-		struct dirent *ent;
-		if ((dir = opendir(path)) != NULL) {
-
-			// Files
-			LUA->CreateTable();
-
-			// Folders
-			LUA->CreateTable();
-
-			// The indices in tables, used for a simple table.insert
-			int file_idx = 1;
-			int dir_idx = 1;
-
-			while ((ent = readdir (dir)) != NULL) {
-				// Ignore symlinks etc
-
-				if (ent->d_type == DT_REG) {
-					LUA->PushNumber(file_idx++);
-					LUA->PushString(ent->d_name);
-					LUA->SetTable(-4);
-				}
-				else if (ent->d_type == DT_DIR && strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
-					LUA->PushNumber(dir_idx++);
-					LUA->PushString(ent->d_name);
-					LUA->SetTable(-3);
-				}
-			}
-			
-			closedir (dir);
-
-			return 2;
-		} else {
-			LUA->PushBool(false);
-			LUA->PushString("Failed to list folder");
-			return 2;
-		}
+		LUA->PushBool(false);
+		LUA->PushString("First parameter not a string");
+		return 2;
 	}
 
-	return 0;
+	const Bootil::BString& folder = LUA->GetString(1);
+	Bootil::String::List list;
+	int files = Bootil::File::GetFilesInFolder(folder, list, false);
+
+	LUA->CreateTable();
+	int i = 1;
+	for (auto f = list.begin(); f != list.end(); ++f) {
+		LUA->PushNumber(i);
+		LUA->PushString((*f).c_str());
+		LUA->SetTable(-3);
+
+		i++;
+	}
+	
+	return 1;
 }
 
 int LuaFunc_ReadFile( lua_State* state )
 {
-	if ( LUA->IsType( 1, Type::STRING ) )
+	if ( !LUA->IsType( 1, Type::STRING ) )
 	{
-		
-		std::ifstream t;
-		t.open(LUA->GetString(1));
-
-		if (t.fail()) {
-			char* err = strerror(errno);
-			LUA->PushBool(false);
-			LUA->PushString(err);
-			return 2;
-		}
-
-		std::string contents((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
-
-		LUA->PushString(contents.c_str());
-
-		return 1;
+		LUA->PushBool(false);
+		LUA->PushString("First parameter not a string");
+		return 2;
 	}
-	LUA->PushBool(false);
-	LUA->PushString("No filename given");
-	return 2;
+
+	Bootil::BString out;
+	if (!Bootil::File::Read(LUA->GetString(1), out)) {
+		LUA->PushBool(false);
+		LUA->PushString(Bootil::Platform::LastError().c_str());
+		return 2;
+	}
+	
+	LUA->PushString(out.c_str());
+	return 1;
 }
 
 int LuaFunc_DeleteFile( lua_State* state )
 {
-	if ( LUA->IsType( 1, Type::STRING ) )
+	if ( !LUA->IsType( 1, Type::STRING ) )
 	{
-		int ret = remove(LUA->GetString(1));
-
-		LUA->PushBool(true);
-		return 1;
+		LUA->PushBool(false);
+		LUA->PushString("First parameter not a string");
+		return 2;
 	}
+
+	if (!Bootil::File::RemoveFile(LUA->GetString(1))) {
+		LUA->PushBool(false);
+		LUA->PushString(Bootil::Platform::LastError().c_str());
+		return 2;
+	}
+
 	return 0;
 }
 
 int LuaFunc_WriteToFile( lua_State* state )
 {
-	if ( LUA->IsType( 1, Type::STRING ) && LUA->IsType( 2, Type::STRING ) )
+	if ( !LUA->IsType( 1, Type::STRING ) )
 	{
-		std::ofstream t;
-		t.open(LUA->GetString(1));
-
-		if (t.fail()) {
-			char* err = strerror(errno);
-			LUA->PushBool(false);
-			LUA->PushString(err);
-			return 2;
-		}
-
-		t << LUA->GetString(2);
-		t.close();
-
-		LUA->PushBool(true);
-
-		return 1;
+		LUA->PushBool(false);
+		LUA->PushString("First parameter not a string");
+		return 2;
 	}
-	LUA->PushBool(false);
-	LUA->PushString("No filename/content given");
-	return 2;
+	if ( !LUA->IsType( 2, Type::STRING ) )
+	{
+		LUA->PushBool(false);
+		LUA->PushString("Second parameter not a string");
+		return 2;
+	}
+	
+	if (!Bootil::File::Write(LUA->GetString(1), LUA->GetString(2))) {
+		LUA->PushBool(false);
+		LUA->PushString(Bootil::Platform::LastError().c_str());
+		return 2;
+	}
+
+	return 0;
 }
 
 int LuaFunc_IsFolder( lua_State* state )
 {
-	if ( LUA->IsType( 1, Type::STRING ) )
+	if ( !LUA->IsType( 1, Type::STRING ) )
 	{
-		struct stat buf;
-
-		int ret = stat(LUA->GetString(1), &buf);
-
-		LUA->PushBool(S_ISDIR(buf.st_mode));
-		return 1;
+		LUA->PushBool(false);
+		LUA->PushString("First parameter not a string");
+		return 2;
 	}
-	return 0;
+
+	LUA->PushBool(Bootil::File::IsFolder(LUA->GetString(1)));
+	return 1;
 }
 
 int LuaFunc_Exists( lua_State* state )
 {
-	if ( LUA->IsType( 1, Type::STRING ) )
+	if ( !LUA->IsType( 1, Type::STRING ) )
 	{
-		struct stat buf;
+		LUA->PushBool(false);
+		LUA->PushString("First parameter not a string");
+		return 2;
+	}
 
-		int ret = stat(LUA->GetString(1), &buf);
+	LUA->PushBool(Bootil::File::Exists(LUA->GetString(1)));
+	return 1;
+}
 
-		LUA->PushBool(S_ISDIR(buf.st_mode) || S_ISREG(buf.st_mode));
-		return 1;
+int LuaFunc_CreateFolder( lua_State* state )
+{
+	
+	if ( !LUA->IsType( 1, Type::STRING ) )
+	{
+		LUA->PushBool(false);
+		LUA->PushString("First parameter not a string");
+		return 2;
+	}
+	
+	if (!Bootil::File::CreateFolder(LUA->GetString(1))) {
+		LUA->PushBool(false);
+		LUA->PushString(Bootil::Platform::LastError().c_str());
+		return 2;
 	}
 
 	return 0;
 }
 
-int LuaFunc_CreateFolder( lua_State* state )
-{
-	if ( LUA->IsType( 1, Type::STRING ) )
-	{
-
-#ifdef __linux__
-		int ret = mkdir(LUA->GetString(1), 0700);
-#endif // __linux__
-#ifdef _WIN32
-		int ret = mkdir(LUA->GetString(1));
-#endif // _WIN32
-
-		if (ret != 0) {
-			LUA->PushBool(false);
-
-			std::ostringstream out;  
-			out << "Error code: " << ret;
-			LUA->PushString(out.str().c_str());
-			return 2;
-		}
-
-		LUA->PushBool(true);
-		return 1;
-	}
-	
-	LUA->PushBool(false);
-	LUA->PushString("No name given");
-	return 2;
-}
-
-const char* ExecSystemCmd(const char* cmd) {
-
-#ifdef _WIN32
-	FILE* pipe = _popen(cmd, "r");
-#else
-	FILE* pipe = popen(cmd, "r");
-#endif
-
-    if (!pipe) {
-		int errornum = errno;
-
-		std::ostringstream s;
-		s << "ERROR " << errornum;
-
-		return s.str().c_str();
-	}
-    char buffer[128];
-    std::string result = "";
-    while(!feof(pipe)) {
-    	if(fgets(buffer, 128, pipe) != NULL)
-    		result += buffer;
-    }
-
-#ifdef _WIN32
-	_pclose(pipe);
-#else
-	pclose(pipe);
-#endif
-
-	return result.c_str();
-}
-
-int LuaFunc_RunSystemCommand( lua_State* state )
-{
-	LUA->CheckString( 1 );
-	const char* cmd = LUA->GetString( 1 );
-	const char* result = ExecSystemCmd(cmd);
-	LUA->PushString( result );
-
-	return 1;
-}
-
-// RunSystemCommand that uses "system" and thus can only return an integer (the return code of the system func call)
-int LuaFunc_RunSimpleSystemCommand( lua_State* state )
-{
-	LUA->CheckString( 1 );
-
-	const char* cmd = LUA->GetString( 1 );
-	int result = system(cmd);
-
-	LUA->PushNumber(result);
-	return 1;
-}
+#define LUA_TABLE_SET_CFUNC(name, func) \
+	LUA->PushString( name ); \
+	LUA->PushCFunction( func ); \
+	LUA->SetTable( -3 ); \
 
 GMOD_MODULE_OPEN()
 {
 
 	LUA->PushSpecial( GarrysMod::Lua::SPECIAL_GLOB );
-	
+
 	LUA->PushString( "gaceio" );
 	LUA->CreateTable();
 
 	// File IO
 
-	LUA->PushString( "List" );
-	LUA->PushCFunction( LuaFunc_ListDir );
-	LUA->SetTable( -3 );
+	LUA_TABLE_SET_CFUNC( "List", LuaFunc_ListDir );
 
 	LUA->PushString( "Read" );
 	LUA->PushCFunction( LuaFunc_ReadFile );
@@ -283,19 +172,9 @@ GMOD_MODULE_OPEN()
 	LUA->PushString( "Exists" );
 	LUA->PushCFunction( LuaFunc_Exists );
 	LUA->SetTable( -3 );
-	
+
 	LUA->PushString( "CreateFolder" );
 	LUA->PushCFunction( LuaFunc_CreateFolder );
-	LUA->SetTable( -3 );
-
-	// System IO
-
-	LUA->PushString("RunSystemCommand");
-	LUA->PushCFunction( LuaFunc_RunSystemCommand );
-	LUA->SetTable( -3 );
-	
-	LUA->PushString("RunSimpleSystemCommand");
-	LUA->PushCFunction( LuaFunc_RunSimpleSystemCommand );
 	LUA->SetTable( -3 );
 
 	LUA->SetTable( -3 );
